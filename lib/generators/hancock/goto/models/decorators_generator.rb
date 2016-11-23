c387bec86270689c28378207d9e72b549243ad82
Alexander Kiseliev <i43ack@gmail.com>
2016-11-23 10:36:40 +0300

    generators upd; dependencies

--- lib/generators/hancock/goto/models/decorators_generator.rb
+++ lib/generators/hancock/goto/models/decorators_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators'
 module Hancock::Goto::Models
   class DecoratorsGenerator < Rails::Generators::Base
     source_root File.expand_path('../../../../../../app/models/concerns/hancock/goto/decorators', __FILE__)
-    argument :Models, type: :array, default: []
+    argument :models, type: :array, default: []
 
     desc 'Hancock::Goto Models decorators generator'
     def decorators
