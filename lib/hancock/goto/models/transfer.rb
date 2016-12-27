3c1cf65fe91cdf1f3b1d516d14c0e11e1fc02be4
Alexander Kiseliev <i43ack@gmail.com>
2016-12-27 14:33:46 +0300

    cancancan fixes

--- lib/hancock/goto/models/transfer.rb
+++ lib/hancock/goto/models/transfer.rb
@@ -10,17 +10,18 @@ module Hancock::Goto
 
       included do
 
-        def self.manager_can_default_actions
-          [:show, :read].freeze
-        end
-        def self.manager_cannot_actions
-          [:new, :create, :edit, :update].freeze
+        # def self.manager_can_default_actions
+        #   # [:show, :read].freeze
+        #   super - [:new, :create, :delete, :destroy]
+        # end
+        def self.manager_cannot_add_actions
+          [:new, :create, :delete, :destroy, :edit, :update]
         end
 
         def self.manager_can_add_actions
           ret = []
           ret << :model_settings if Hancock::Goto.config.model_settings_support
-          ret << :model_accesses if Hancock::Goto.config.user_abilities_support
+          # ret << :model_accesses if Hancock::Goto.config.user_abilities_support
           ret += [:comments, :model_comments] if Hancock::Goto.config.ra_comments_support
           ret.freeze
         end
--- lib/hancock/goto/version.rb
+++ lib/hancock/goto/version.rb
@@ -1,5 +1,5 @@
 module Hancock
   module Goto
-    VERSION = "1.0.1.3".freeze
+    VERSION = "1.0.2".freeze
   end
 end
