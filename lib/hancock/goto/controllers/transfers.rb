3e0e2fb5bd5cc5253261f309870614c1049c907f
Alexander Kiseliev <i43ack@gmail.com>
2017-02-20 11:49:31 +0300

    typo

--- lib/hancock/goto/controllers/transfers.rb
+++ lib/hancock/goto/controllers/transfers.rb
@@ -4,23 +4,34 @@ module Hancock::Goto
       extend ActiveSupport::Concern
 
       def index
-        @url = Addressable::URI.heuristic_parse(params[:url]).to_s rescue nil
+        begin
+          @url = Addressable::URI.heuristic_parse(params[:url])
+          @target_url = @url.to_s
+        rescue
+          redirect_to root_path
+          return false
+        end
+
         _params = params.clone
         _session = session.clone
         _request = request.clone
         Thread.new do
-          referer = (_request.referer ? Addressable::URI.parse(_request.referer) : nil) rescue nil
+          begin
+            referer = (_request.referer ? Addressable::URI.parse(_request.referer) : nil) rescue nil
 
-          @transfer = transfer_class.new
-          @transfer.recieved_url = _params[:url]
-          @transfer.url = @url.to_s
-          @transfer.host = @url.host.to_s if @url
-          @transfer.referer = referer.to_s
-          @transfer.source_ip = _request.env['HTTP_X_FORWARDED_FOR'] || _request.remote_ip
-          if Hancock::Goto.mongoid?
-            @transfer.set_session(_session)
+            @transfer = transfer_class.new
+            @transfer.recieved_url = _params[:url]
+            @transfer.url = @url.to_s
+            @transfer.host = @url.host.to_s if @url
+            @transfer.referer = referer.to_s
+            @transfer.source_ip = _request.env['HTTP_X_FORWARDED_FOR'] || _request.remote_ip
+            if Hancock::Goto.mongoid?
+              @transfer.set_session(_session)
+            end
+            @transfer.save
+          rescue Exception => ex
+            puts ex.inspect
           end
-          @transfer.save
         end
 
         case Hancock::Goto.config.method.to_sym
