a34fc0993d358e0b02b6a6e465457dc56d7af77f
Alexander Kiseliev <i43ack@gmail.com>
2017-05-29 15:01:03 +0300

    renaming

--- app/helpers/hancock/goto/transfers_helper.rb
+++ app/helpers/hancock/goto/transfers_helper.rb
@@ -1,12 +1,20 @@
-module Hancock::Goto::GotoHelper
+module Hancock::Goto::TransfersHelper
+  include ::ActionView::Helpers::UrlHelper
 
   def link_goto(name = nil, options = nil, html_options = nil, &block)
     html_options, options, name = options, name, block if block_given?
     options ||= {}
     html_options ||= {}
 
+    # include
+
     target_url = options.is_a?(Hash) ? url_for(options.reverse_merge({only_path: false})) : options
-    goto_url = url_for({controller: "hancock/goto/transfers", action: 'index', url: target_url})
+    if target_url.is_a?(String) and target_url.starts_with?("//")
+      request_scheme = "http" unless respond_to?(:request_scheme)
+      target_url = "#{request_scheme}:#{target_url}"
+    end
+    # goto_url = url_for(controller: "hancock/goto/transfers", action: 'index', url: target_url)
+    goto_url = Rails.application.routes.url_helpers.hancock_goto_path(url: target_url)
     if html_options.is_a?(Hash)
       html_options['data-href'] ||= target_url
       html_options['data-gotohref'] ||= goto_url
